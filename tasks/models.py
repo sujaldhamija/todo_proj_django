cf6b618297018cfa73891d91e7351a665a90957b
Sujal Dhamija <sujaldhamija@gmail.com>
2025-08-31 10:14:03 +0530

    Update Task Table

--- tasks/models.py
+++ tasks/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=200)
     is_done = models.BooleanField(default=False)
 
